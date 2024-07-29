60b130095789d3ab1b6f2f2f1f632c019ad8c2e2
Abrham <abrsh6266@gmail.com>
2024-07-28 23:30:39 -0800

    configuring react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const Home = () => {
+    return <h1>Welcome</h1>;
+  };
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!!</h1>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
