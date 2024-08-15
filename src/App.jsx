385a1c918514216eeea39ee132690ae727039e8c
Abrham <abrsh6266@gmail.com>
2024-08-15 08:14:51 -0800

    conditions for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Homepage from "./components/Homepage/Homepage";
 import Login from "./components/Users/Login";
 import UserProfile from "./components/Users/UserProfile";
 import PublicNavbar from "./components/Navbar/PublicNavbar";
+import { useSelector } from "react-redux";
+import PrivateNavbar from "./components/Navbar/PrivateNavbar copy";
 
 export default function App() {
+  const { userAuth } = useSelector((state) => state?.users);
   return (
     <BrowserRouter>
       {/* Navbar */}
-      <PublicNavbar />
+      {userAuth?.userInfo?.token ? <PrivateNavbar /> : <PublicNavbar />}
       <Routes>
         <Route path="" element={<Homepage />}></Route>
         <Route path="/login" element={<Login />}></Route>
