d76219e8529569ac6288468b9be4751c3f5d5cc2
Abrham <abrsh6266@gmail.com>
2024-08-15 08:01:54 -0800

    public navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Login from "./components/Users/Login";
 import UserProfile from "./components/Users/UserProfile";
+import PublicNavbar from "./components/Navbar/PublicNavbar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* Navbar */}
+      <PublicNavbar />
       <Routes>
         <Route path="" element={<Homepage />}></Route>
         <Route path="/login" element={<Login />}></Route>
